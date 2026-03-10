f41b368ecfa151352c62be7d658c0f8a3670e479
Yazouzou <azouzou.yousra@gmail.com>
2026-03-10 12:42:39 +0000

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Search from "./pages/Search"
-import BookDetails from "./pages/BookDetails"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-
 function App() {
   return (
-    <BrowserRouter>
-
-      <Navbar />
-
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/search" element={<Search />} />
-
-        <Route path="/book/:id" element={<BookDetails />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-      </Routes>
-
-    </BrowserRouter>
+    <div className="text-4xl text-center mt-20">
+      Book Library Working ✅
+    </div>
   )
 }
 
